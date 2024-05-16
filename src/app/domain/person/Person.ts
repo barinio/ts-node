e50305f80ea8731a49c1775a366e20ef03bfbe3d
Ваня <7328652@gmail.com>
2024-05-16 21:21:33 +0300

    Response structure

--- src/app/domain/person/Person.ts
+++ src/app/domain/person/Person.ts
@@ -1,5 +1,7 @@
 import { Body, Get, JsonController, Param, Post } from "routing-controllers";
 import { IPerson } from "./Person.types";
+import { ApiResponse } from "helpers/ApiResponse";
+import { ApiError } from "helpers/ApiError";
 
 const storeData: IPerson[] = [];
 
@@ -7,20 +9,27 @@ const storeData: IPerson[] = [];
 export default class Person {
   @Get()
   async getAll() {
-    return storeData;
+    return new ApiResponse(true, storeData);
   }
 
   @Get("/:id")
   async getById(@Param("id") id: number): Promise<IPerson | {}> {
     const person = storeData.find((item) => item.id === id);
 
-    return person || {};
+    if (!person) {
+      throw new ApiError(404, {
+        code: "PERSON_NOT_FOUND",
+        message: `Person with id ${id} not found`
+      });
+    }
+
+    return new ApiResponse(true, person);
   }
 
   @Post()
   async setPerson(@Body() body: IPerson) {
     storeData.push(body);
 
-    return true;
+    return new ApiResponse(true, "Person successfully created");
   }
 }
---
+++ src/helpers/ApiError.ts
@@ -0,0 +1,28 @@
+import { ValidationError } from "class-validator";
+import { HttpError } from "routing-controllers";
+
+interface MessageInterface {
+  status: number;
+  message: string;
+  code?: string;
+  errors?: ValidationError[];
+}
+
+export class ApiError extends HttpError {
+  protected error: MessageInterface;
+  public removeLog: boolean;
+
+  constructor(status = 500, error: Omit<MessageInterface, "status">) {
+    super(status);
+
+    this.error = { ...error, status, code: error.code || "INTERNAL_ERROR" };
+
+    this.name = "ApiError";
+
+    this.message = error.message || "";
+  }
+
+  public toJSON = (): MessageInterface => {
+    return this.error;
+  };
+}
